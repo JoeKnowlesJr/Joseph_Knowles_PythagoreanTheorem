62e684d6e911a000be58c7e210df8d15c6009e7c
Joe Knowles <50335840+JoeKnowlesJr@users.noreply.github.com>
2019-06-08 21:32:30 -0500

    Add files via upload

---
+++ PythagoreanTest.java
@@ -0,0 +1,6 @@
+public class PythagoreanTest {
+    public static void main(String[] args) {
+        Pythagorean pythagorean = new Pythagorean();
+        System.out.println(pythagorean.calculateHypotenuse(3, 4));
+    }
+}
